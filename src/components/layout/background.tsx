cff0801371e1cc5683d1212922a9fd9ca5d3e00b
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-04-10 16:57:17 +0900

    背景に42tokyoロゴの画像を配置した

--- src/components/layout/background.tsx
+++ src/components/layout/background.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Image } from '@chakra-ui/react';
 
 export const Background = () => {
     return (
-        <Box bg="gray.700" minHeight="100vh">
+        <Box bg="gray.700" minHeight="100vh" position="relative">
+            <Image
+                src="/42tokyo.svg"// publicディレクトリ内のファイルは、アプリケーションのルートパスからアクセスできる
+                alt="42tokyo"
+                position="absolute"
+                top="15%"
+                left="50%"
+                transform="translateX(-50%)"
+                width="200px"
+                height="auto"
+                opacity={0.5}
+                filter="invert(100%)" // 色を反転
+            />
         </Box>
     );
 };
