33cf7c5e640527f2dc557ed33d9e402b1c9e6b7f
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-11 00:01:07 +0900

    :dizzy:[update][送金]hover時のボタンcolor, 背景色指定の記述調整など

--- src/components/send/SendInput.tsx
+++ src/components/send/SendInput.tsx
@@ -3,7 +3,7 @@ import { Box, Input } from "@chakra-ui/react";
 export const SendInput = () => {
     return (
         <Box m="0 11px" pt="190px">
-          <Input placeholder="送金したい人のintra名を入力" background="#ffffff" color="#000000" border="none" borderRadius="6px" />
+          <Input placeholder="送金したい人のintra名を入力" bg="#ffffff" color="#000000" border="none" borderRadius="6px" />
         </Box>
     );
 }
--- src/components/send/sendConfirm.tsx
+++ src/components/send/sendConfirm.tsx
@@ -1,6 +1,5 @@
 import {Box, Button, Avatar, Text, Flex} from "@chakra-ui/react";
 
-
 export const SendConfirm = () => {
     return (
         <Box pt="120px" textAlign="center">
@@ -8,8 +7,8 @@ export const SendConfirm = () => {
             <Avatar name="ymatsui" src='//ui-avatars.com/api/?name=Yushi matsui&background=e3ad03&color=ffffff' />
             <Text ml={"0.5em"} fontSize="32px" color="white">ymatsui</Text>
           </Flex>
-            <Text fontSize="90px" color="white" lineHeight="1em" mb="65px">3pt</Text>
-            <Button width="160px" height="60px" borderRadius="20px" background="#EF4C4C" fontSize="40px" color="#ffffff">送る</Button>
+            <Text fontSize="90px" color="white" lineHeight="1em" mb="65px"><a href="#">3pt</a></Text>
+            <Button width="160px" height="60px" borderRadius="20px" bg="#EF4C4C" _hover={{ bg: '#EF4C4C' }} fontSize="40px" fontWeight="normal" color="#ffffff">送る</Button>
         </Box>
     );
 }
