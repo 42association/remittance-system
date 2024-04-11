1bbceced1c63bb3c735952b5eea11553f72a055e
masaru ogawa <masaruo@gmail.com>
2024-04-11 19:22:02 +0900

    NavBar Link function added

--- src/components/icons/NavItem.tsx
+++ src/components/icons/NavItem.tsx
@@ -1,25 +1,26 @@
-import { VStack, IconButton, Text } from "@chakra-ui/react";
+import { VStack, IconButton, Text, Link } from "@chakra-ui/react";
 import React from "react";
 import { IconType } from 'react-icons';
 
 interface NavButtonProps {
 	icon: IconType;
 	label: string;
-	onClick: () => void;
+	href: string,
 }
 
-const NavButton: React.FC<NavButtonProps> = ({ icon, label, onClick }) => {
+const NavButton: React.FC<NavButtonProps> = ({ icon, label, href }) => {
 	return (
-	  <VStack spacing={1}>
+		<VStack spacing={1}>
+		<Link href={href}>
 		<IconButton
 		  icon={React.createElement(icon)}
 		  aria-label={label}
 		  fontSize="30px"
 		  size="xl"
 		  colorScheme="grey.800"
-		  onClick={onClick}
 		/>
 		<Text fontSize="sm">{label}</Text>
+		</Link>
 	  </VStack>
 	);
   };
--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -2,22 +2,8 @@
 import { Box, Flex } from '@chakra-ui/react';
 import NavButton from '@/src/components/icons/NavItem';
 import { FaHome, FaPiggyBank, FaCoins, FaList } from 'react-icons/fa';
-// import React from 'react';
-// import { Box, Flex, Link, Text } from '@chakra-ui/react';
-
-// const menuItems = [
-//     { label: 'ホーム', href: '/' },
-//     { label: '送金', href: '/send' },
-//     { label: '両替', href: '/exchange' },
-//     { label: '順位', href: '/rank' },
-// ];
 
 const NavBar: React.FC = () => {
-    const handleHomeClick = () => { console.log("home clicked") };
-    const handleSendClick = () => { console.log("send clicked") };
-    const handleExchangeClick = () => { console.log("exchange clicked") };
-    const handleRankClick = () => { console.log("rank clicked") };
-
     return (
         <Box position="fixed" bottom="0" left="0" right="0">
             <Flex
@@ -30,27 +16,13 @@ const NavBar: React.FC = () => {
                 color="white"
                 w="full"
                 >
-            <NavButton icon={FaHome} label='ホーム' onClick={handleHomeClick} />
-            <NavButton icon={FaPiggyBank} label='送金' onClick={handleSendClick} />
-            <NavButton icon={FaCoins} label='両替' onClick={handleExchangeClick} />
-            <NavButton icon={FaList} label='順位' onClick={handleRankClick} />
+            <NavButton icon={FaHome} label='ホーム' href='/' />
+            <NavButton icon={FaPiggyBank} label='送金' href='/send' />
+            <NavButton icon={FaCoins} label='両替' href='/exchange' />
+            <NavButton icon={FaList} label='順位' href='rank' />
             </Flex>
         </Box>
     );
 };
 
 export default NavBar;
-
-// export const Navbar = () => {
-//     return (
-//         <Box bg="gray.900" color="white" py={4} position="fixed" bottom={0} left={0} right={0}>
-//             <Flex justifyContent="space-around">
-//                 {menuItems.map((item, index) => (
-//                     <Link href={item.href} key={index}>
-//                         <Text>{item.label}</Text>
-//                     </Link>
-//                 ))}
-//             </Flex>
-//         </Box>
-//     );
-// };
