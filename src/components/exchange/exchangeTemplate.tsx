d24dc897baf65030c1fb22b84dd43586caae1437
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-04-11 19:21:06 +0900

    comming soon を追加

--- src/components/exchange/exchangeTemplate.tsx
+++ src/components/exchange/exchangeTemplate.tsx
@@ -1,10 +1,30 @@
-import { Box, Heading } from "@chakra-ui/react";
-
+import { Box, Text } from "@chakra-ui/react";
 
 export const ExchangeTemplate = () => {
     return (
-        <Box>
-            <Heading> 両替</ Heading>
+        <Box
+            position="absolute"
+            width="100%"
+            height="100%"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+        >
+            <Box
+                textAlign="center"
+                marginTop="-15%"
+            >
+                <Text
+                    color="white"
+                    fontSize="5xl"
+                    whiteSpace="nowrap"
+                    overflow="hidden"
+                    textOverflow="ellipsis"
+                    transform="rotate(-15deg)"
+                >
+                    COMMING SOON
+                </Text>
+            </Box>
         </Box>
     );
-}
+};
