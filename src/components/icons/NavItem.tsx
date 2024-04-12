d6b6c929fcda120983f56cfc2b6e0373ff286928
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-12 20:10:07 +0900

    :dizzy:[update]グローバルナビstyle調整(css module)

--- src/components/icons/NavItem.tsx
+++ src/components/icons/NavItem.tsx
@@ -2,6 +2,7 @@ import { VStack, IconButton, Text, Link } from "@chakra-ui/react";
 import React from "react";
 import { IconType } from 'react-icons';
 import { Icon } from '@chakra-ui/react'
+import styles from "./NavItem.module.css";
 
 interface NavButtonProps {
 	icon: IconType;
@@ -11,10 +12,10 @@ interface NavButtonProps {
 
 const NavButton: React.FC<NavButtonProps> = ({ icon, label, href }) => {
 	return (
-		<Link href={href}>
-			<VStack spacing={1} align="center">
-				<Icon as={icon} boxSize={10} color="#b0b0b0" />
-				<Text fontSize="sm" textAlign="center" color="#b0b0b0">
+		<Link href={href} className={styles.className}>
+			<VStack spacing={1}>
+				<Icon as={icon} boxSize={9} />
+				<Text fontSize="sm" textAlign="center">
 					{label}
 				</Text>
 			</VStack>
